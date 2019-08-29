5afa34f1b2ab02886b199de1e182765bcbcacbe4
Ike <ikeholzmann@gmail.com>
2019-08-29 15:30:03 -0700

    Python: simplify code/use arrays

--- python/madlib.py
+++ python/madlib.py
@@ -1,31 +1,40 @@
 from termcolor import colored, cprint
 
-one = raw_input("Enter a plural noun: ")
-two = raw_input("Enter a place: ")
-three = raw_input("Enter a noun: ")
-four = raw_input("Enter a plural noun: ")
-five = raw_input("Enter a noun: ")
-six = raw_input("Enter an adjective: ")
-seven = raw_input("Enter a verb: ")
-eight = raw_input("Enter a number: ")
-nine = raw_input("Enter an adjective: ")
-ten = raw_input("Enter a body part: ")
-eleven = raw_input("Enter a verb: ")
+userInput = [
+    raw_input("Enter a plural noun: "),
+    raw_input("Enter a place: "),
+    raw_input("Enter a noun: "),
+    raw_input("Enter a plural noun: "),
+    raw_input("Enter a noun: "),
+    raw_input("Enter an adjective: "),
+    raw_input("Enter a verb: "),
+    raw_input("Enter a number: "),
+    raw_input("Enter an adjective: "),
+    raw_input("Enter a body part: "),
+    raw_input("Enter a verb: ")
+]
 
+storyText = [
+    "  Two "+colored(userInput[0], attrs=['bold'])+", both alike in dignity,",
+    "  In fair "+colored(userInput[1], attrs=['bold'])+", where we lay our scene,",
+    "  From ancient "+colored(userInput[2], attrs=['bold'])+" break to new mutiny,",
+    "  Where civil blood makes civil hands unclean.",
+    "  From forth the fatal loins of these two foes",
+    "  A pair of star-cross`d "+colored(userInput[3], attrs=['bold'])+" take their life;",
+    "  Whole misadventured piteous overthrows",
+    "  Do with their "+colored(userInput[4], attrs=['bold'])+" bury their parents` strife.",
+    "  The fearful passage of their "+colored(userInput[5], attrs=['bold'])+" love,",
+    "  And the continuance of their parents` rage,",
+    "  Which, but their children`s end, nought could "+colored(userInput[6], attrs=['bold'])+",",
+    "  Is now the "+colored(userInput[7], attrs=['bold'])+" hours` traffic of our stage;",
+    "  The which if you with "+colored(userInput[8], attrs=['bold'])+" "+colored(userInput[9], attrs=['bold'])+" attend,",
+    "  What here shall "+colored(userInput[10], attrs=['bold'])+", our toil shall strive to mend.",
+]
+
+i = 0
 print("*_______________________________________________________________________*")
-print("  Two "+colored(one, attrs=['bold'])+", both alike in dignity,")
-print("  In fair "+colored(two, attrs=['bold'])+", where we lay our scene,")
-print("  From ancient "+colored(three, attrs=['bold'])+" break to new mutiny,")
-print("  Where civil blood makes civil hands unclean.")
-print("  From forth the fatal loins of these two foes")
-print("  A pair of star-cross`d "+colored(four, attrs=['bold'])+" take their life;")
-print("  Whole misadventured piteous overthrows")
-print("  Do with their "+colored(five, attrs=['bold'])+" bury their parents` strife.")
-print("  The fearful passage of their "+colored(six, attrs=['bold'])+" love,")
-print("  And the continuance of their parents` rage,")
-print("  Which, but their children`s end, nought could "+colored(seven, attrs=['bold'])+",")
-print("  Is now the "+colored(eight, attrs=['bold'])+" hours` traffic of our stage;")
-print("  The which if you with "+colored(nine, attrs=['bold'])+" "+colored(ten, attrs=['bold'])+" attend,")
-print("  What here shall "+colored(eleven, attrs=['bold'])+", our toil shall strive to mend.")
+while i < len(storyText):
+    print(storyText[i])
+    i += 1
 print("*_______________________________________________________________________*")
 print("Romeo and Juliet Prologue Source: https://www.madtakes.com/libs/186.html")
